30aa77d8246d438aaec8dcb275fb503cb3c47d24
Jack Gilcrest <me@jp4g.llc>
2023-06-09 15:53:26 -0400

    base case circuits mock proven

--- src/pse_halo2/test/x.rs
+++ src/pse_halo2/test/x.rs
@@ -1,10 +1,13 @@
 use crate::pse_halo2::circuit_translator::NoirHalo2Translator;
+use crate::dimension_measure::DimensionMeasurement;
 use acvm::acir::circuit::Circuit;
 use acvm::acir::native_types::WitnessMap;
 use pse_halo2wrong::curves::bn256::Fr;
+use pse_halo2wrong::halo2::dev::MockProver;
 
 use std::fs::File;
-use std::io::{self, Read, Write};
+use std::io::Read;
+use std::marker::PhantomData;
 
 /**
  * Read an example circuit & pre-solved witness from a stored file
@@ -33,8 +36,8 @@ mod test {
     #[test]
     fn deserialize() {
         let (circuit, witness) = get_circuit("add").unwrap();
-        println!("Add Circuit: {:?}", circuit);
-        println!("Add Witnesses: {:?}", witness);
+        // println!("Add Circuit: {:?}", circuit);
+        // println!("Add Witnesses: {:?}", witness);
     }
 
     #[test]
@@ -45,11 +48,42 @@ mod test {
         // // file.write_all()?;
     }
 
-    fn test_add_circuit() {
-        // let translator = NoirHalo2Translator::<Fr> {
-        //     circuit: circuit.clone(),
-        //     witness_values,
-        //     _marker: PhantomData::<Fr>,
-        // };
+    #[test]
+    fn test_add_circuit_success() {
+        // get circuit
+        let (circuit, witness) = get_circuit("add").unwrap();
+
+        // instantiate halo2 circuit
+        let translator = NoirHalo2Translator::<Fr> {
+            circuit: circuit.clone(),
+            witness_values: witness.clone(),
+            _marker: PhantomData::<Fr>,
+        };
+        let dimension = DimensionMeasurement::measure(&translator).unwrap();
+
+        // instance value (known to be equal to 7)
+        let instance = vec![Fr::from_raw([7u64, 0, 0, 0])];
+
+        // run mock prover expecting success
+        let prover = MockProver::run(dimension.k(), &translator, vec![instance]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
+    }
+
+    #[test]
+    fn test_bit_and_circuit_success() {
+        // get circuit
+        let (circuit, witness) = get_circuit("bit_and").unwrap();
+
+        // instantiate halo2 circuit
+        let translator = NoirHalo2Translator::<Fr> {
+            circuit: circuit.clone(),
+            witness_values: witness.clone(),
+            _marker: PhantomData::<Fr>,
+        };
+        let dimension = DimensionMeasurement::measure(&translator).unwrap();
+
+        // run mock prover expecting success
+        let prover = MockProver::run(dimension.k(), &translator, vec![vec![]]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
     }
 }
