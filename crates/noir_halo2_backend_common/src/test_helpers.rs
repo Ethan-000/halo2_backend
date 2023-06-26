6294e5a6b22c2b23a7b954e20951948e4624dbf2
ethan-000 <ethan_000@outlook.com>
2023-06-26 20:01:47 +0100

    fix

--- crates/noir_halo2_backend_common/src/test_helpers.rs
+++ crates/noir_halo2_backend_common/src/test_helpers.rs
@@ -130,20 +130,19 @@ pub fn assert_nargo_cmd_works(cmd_name: &str, test_test_program_dir: &PathBuf) {
 }
 
 pub fn install_nargo(backend: &'static str) {
-    if std::fs::symlink_metadata("../noir").is_err() {
-        // Clone noir into repo
-        Command::new("git")
-            .current_dir(fs::canonicalize("../").unwrap())
-            .arg("clone")
-            .arg("https://github.com/Ethan-000/noir")
-            .arg("--branch")
-            .arg("add_halo2_backend")
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap();
-        format!("\nInstalling {backend}. This may take a few moments.",);
-    }
+    // Clone noir into repo
+    Command::new("git")
+        .current_dir(fs::canonicalize("../").unwrap())
+        .arg("clone")
+        .arg("https://github.com/Ethan-000/noir")
+        .arg("--branch")
+        .arg("add_halo2_backend")
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+    format!("\nInstalling {backend}. This may take a few moments.",);
+
     // Install specified backend into noir
     Command::new("cargo")
         .current_dir(fs::canonicalize("../noir/crates/nargo_cli").unwrap())
